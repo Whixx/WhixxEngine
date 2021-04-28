c769a45503ae09543ccd2b6049bd2d6320565eae
William <ville.or@hotmail.com>
2021-04-28 20:48:00 +0200

    Depth!

--- WhixxEngine/Renderer.h
+++ WhixxEngine/Renderer.h
@@ -3,56 +3,42 @@
 class Renderer
 {
 private:
-	static const int frameBufferCount = 3; // number of buffers we want, 2 for double buffering, 3 for tripple buffering
+	static const int frameBufferCount = 3;
+	int frameIndex;
 
-	ID3D12Device* device; // direct3d device
+	ID3D12Device* device;
+	IDXGISwapChain3* swapChain;
+	ID3D12CommandQueue* commandQueue;
+	ID3D12CommandAllocator* commandAllocator[frameBufferCount];
+	ID3D12GraphicsCommandList* commandList;
+	ID3D12PipelineState* pipelineStateObject;
+	ID3D12RootSignature* rootSignature;
 
-	IDXGISwapChain3* swapChain; // swapchain used to switch between render targets
+	ID3D12Fence* fence[frameBufferCount];
+	HANDLE fenceEvent;
+	UINT64 fenceValue[frameBufferCount];
 
-	ID3D12CommandQueue* commandQueue; // container for command lists
+	int rtvDescriptorSize;
 
-	ID3D12DescriptorHeap* rtvDescriptorHeap; // a descriptor heap to hold resources like the render targets
+	D3D12_VIEWPORT viewport;
+	D3D12_RECT scissorRect;
 
-	ID3D12Resource* renderTargets[frameBufferCount]; // number of render targets equal to buffer count
+	ID3D12DescriptorHeap* rtvDescriptorHeap;
+	ID3D12DescriptorHeap* dsDescriptorHeap;
 
-	ID3D12CommandAllocator* commandAllocator[frameBufferCount]; // we want enough allocators for each buffer * number of threads (we only have one thread)
-
-	ID3D12GraphicsCommandList* commandList; // a command list we can record commands into, then execute them to render the frame
-
-	ID3D12Fence* fence[frameBufferCount];    // an object that is locked while our command list is being executed by the gpu. We need as many 
-									 //as we have allocators (more if we want to know when the gpu is finished with an asset)
-
-	HANDLE fenceEvent; // a handle to an event when our fence is unlocked by the gpu
-
-	UINT64 fenceValue[frameBufferCount]; // this value is incremented each frame. each fence will have its own value
-
-	int frameIndex; // current rtv we are on
-
-	int rtvDescriptorSize; // size of the rtv descriptor on the device (all front and back buffers will be the same size)
-				   // function declarations
-
-
-	ID3D12PipelineState* pipelineStateObject; // pso containing a pipeline state
-
-	ID3D12RootSignature* rootSignature; // root signature defines data shaders will access
-
-	D3D12_VIEWPORT viewport; // area that output from rasterizer will be stretched to.
-
-	D3D12_RECT scissorRect; // the area to draw in. pixels outside that area will not be drawn onto
-
-	ID3D12Resource* vertexBuffer; // a default buffer in GPU memory that we will load vertex data for our triangle into
-
+	ID3D12Resource* renderTargets[frameBufferCount];
+	ID3D12Resource* vertexBuffer;
 	ID3D12Resource* indexBuffer;
+	ID3D12Resource* depthStencilBuffer;
 
-	D3D12_VERTEX_BUFFER_VIEW vertexBufferView; // a structure containing a pointer to the vertex data in gpu memory
-											   // the total size of the buffer, and the size of each element (vertex)
+	D3D12_VERTEX_BUFFER_VIEW vertexBufferView;
 	D3D12_INDEX_BUFFER_VIEW indexBufferView;
 
 public:
 	static Renderer* getInstance();
 
-	bool InitD3D(); // initializes direct3d 12
-
+	bool InitD3D(); // initializes Direct3D 12
+	
 	void Update(); // update the game logic
 
 	void UpdatePipeline(); // update the direct3d pipeline (update command lists)
